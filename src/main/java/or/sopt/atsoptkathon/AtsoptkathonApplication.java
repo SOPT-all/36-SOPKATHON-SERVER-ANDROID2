095c40f681a7981c21d8d9dd20de7d1554525bab
gdbs1107 <gdbs1107@naver.com>
2025-05-15 21:20:07 +0900

    init: 프로젝트 초기설정

---
+++ src/main/java/or/sopt/atsoptkathon/AtsoptkathonApplication.java
@@ -0,0 +1,13 @@
+package or.sopt.atsoptkathon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtsoptkathonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AtsoptkathonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/or/sopt/atsoptkathon/AtsoptkathonApplicationTests.java
@@ -0,0 +1,13 @@
+package or.sopt.atsoptkathon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtsoptkathonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
