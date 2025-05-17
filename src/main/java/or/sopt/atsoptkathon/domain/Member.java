6413dee7c6c67d2b6c43299a867e5326dbeaa44a
Hyoeun <joohyoeun@sookmyung.ac.kr>
2025-05-17 23:04:37 +0900

    Entity 생성

---
+++ src/main/java/or/sopt/atsoptkathon/domain/Member.java
@@ -0,0 +1,27 @@
+package or.sopt.atsoptkathon.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import or.sopt.atsoptkathon.domain.common.BaseEntity;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Getter
+@Builder
+public class Member extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+}
