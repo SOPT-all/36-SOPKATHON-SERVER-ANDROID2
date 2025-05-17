82e545fa2d73669ff2f402188d97e992286fb277
gdbs1107 <gdbs1107@naver.com>
2025-05-18 05:19:30 +0900

    fix: 제품 조회 가격 필드 추가

--- src/main/java/or/sopt/atsoptkathon/dto/RegionProductDTO.java
+++ src/main/java/or/sopt/atsoptkathon/dto/RegionProductDTO.java
@@ -2,11 +2,12 @@ package or.sopt.atsoptkathon.dto;
 
 import or.sopt.atsoptkathon.domain.Product;
 
-public record RegionProductDTO(Long id, String productName, String productThumbnail) {
+public record RegionProductDTO(Long id, String productName,int price, String productThumbnail) {
     public static RegionProductDTO from(Product product) {
         return new RegionProductDTO(
                 product.getId(),
                 product.getProductName(),
+                product.getPrice(),
                 product.getDescription()
         );
     }
