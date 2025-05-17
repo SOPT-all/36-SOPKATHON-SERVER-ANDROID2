f84c30fd71052625192145f35e4c3d8b9806025f
gdbs1107 <gdbs1107@naver.com>
2025-05-18 05:01:15 +0900

    fix: 설명 자료형 수정

--- src/main/java/or/sopt/atsoptkathon/domain/Region.java
+++ src/main/java/or/sopt/atsoptkathon/domain/Region.java
@@ -1,9 +1,6 @@
 package or.sopt.atsoptkathon.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -25,6 +22,8 @@ public class Region extends BaseEntity {
 
     private String regionName;
     private String regionSubName;
+
+    @Lob
     private String regionDescription;
 
 }
