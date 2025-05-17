c4f550e2befeb0a8b6a54f9f8e478d7058287a3f
gdbs1107 <gdbs1107@naver.com>
2025-05-17 14:51:51 +0900

    feat: baseEntity 구현

--- src/main/java/or/sopt/atsoptkathon/AtsoptkathonApplication.java
+++ src/main/java/or/sopt/atsoptkathon/AtsoptkathonApplication.java
@@ -2,8 +2,10 @@ package or.sopt.atsoptkathon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class AtsoptkathonApplication {
 
     public static void main(String[] args) {
